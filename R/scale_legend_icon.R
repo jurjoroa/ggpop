32897630eb49bd4258195f67d328714d9379d849
Jorge Roa <jurjoo@gmail.com>
2025-03-21 13:10:15 -0700

    Update scale_legend_icon.R

--- R/scale_legend_icon.R
+++ R/scale_legend_icon.R
@@ -26,7 +26,7 @@ scale_legend_icon <- function(size = 10, ...) {
   # type
   types <- levels(factor(data$type))
   
-  # icon
+  # icons
   icons <- sapply(types, function(t) {
     idx <- which(data$type == t)
     data$icon[idx[1]]
